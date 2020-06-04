7fd5c3ddabfb66526202c13f021cf4cf776f98a0
Vincent Emonet <vincent.emonet@gmail.com>
2020-06-04 10:01:20 +0200

    add extlink package for markdown custom highlights

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -8,6 +8,8 @@
 // See https://docusaurus.io/docs/site-config for all the possible
 // site configuration options.
 
+const extlink = require('remarkable-extlink');
+
 // List of projects/orgs using your project for the users page.
 const users = [
   {
